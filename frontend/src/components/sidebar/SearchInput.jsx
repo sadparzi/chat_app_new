f202dc749e95da5164eb4f15737d94eca469fca1
sadparzi <shaanroj@gmail.com>
2024-12-18 14:02:13 +0530

    added frontend and updated some backend components

---
+++ frontend/src/components/sidebar/SearchInput.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import { TbMoodSearch } from "react-icons/tb";
+
+const SearchInput = () => {
+  return (
+    <form className='flex items-center gap-2'>
+        <input type="text" placeholder='Search...' className='input input-bordered rounded-full' />
+        <button type='submit' className='btn btn-circle bg-sky-500 text-white'>
+        <TbMoodSearch className='w-6 h-6 outline-none'/>
+        </button>
+    </form>
+  )
+}
+
+export default SearchInput
